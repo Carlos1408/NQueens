840db37d9a2a615cbab6ec900b9c58667def1664
CarlosLaptop <carlos.claure.vargas@gmail.com>
2020-11-15 21:17:11 -0400

    Implementacion de backtracking para los ncaballos

---
+++ Horses.java
@@ -0,0 +1,54 @@
+import java.util.Arrays;
+
+public class Horses {
+    private int num_horses;
+    private int[] solution;
+    
+    public Horses(int num_horses) {
+        this.num_horses = num_horses;
+        solution = new int[num_horses];
+        init();
+        String strArray = Arrays.toString(solution);
+        System.out.println(strArray);
+    }
+
+    public void init() {
+        for(int i = 0; i<solution.length; i++)
+            solution[i] = -1;
+    }
+
+    public void searchSolution() {
+        backTracking(solution, 0);
+    }
+    
+    public boolean backTracking(int[] solution, int horse) {
+        boolean succes = false;
+        if(horse < num_horses) {
+            do{
+                solution[horse]++;
+                boolean valid = horseIsValid(solution, horse);
+                System.out.println(Arrays.toString(solution)+" "+ (valid ? "Sol Parcial":"")+(valid && (horse == num_horses - 1) ? "SOLUTION" : ""));
+                if(valid)
+                    succes = backTracking(solution, horse+1);
+            }while(solution[horse] < num_horses-1 && !succes);
+        }
+        solution[horse]=-1;
+        return succes;
+    }
+
+    public boolean horseIsValid(int[] solution, int horse) {
+        boolean ok = true;
+        for (int i = 0; i < horse; i++) {
+            if(solution[i+2]+1==solution[horse] || solution[i+1]+2==solution[horse] || solution[i+2]-1==solution[horse] || solution[i+1]-2==solution[horse]) {
+                ok = false;
+                break;
+            }
+        }
+        return ok;
+    }
+
+    public static void main(String[] args) {
+        Horses horses = new Horses(10);
+        horses.searchSolution();
+    }
+}
--- Queens.java
+++ Queens.java
@@ -22,19 +22,30 @@ public class Queens {
         backTracking(solution, 0);
     }
     
-    public void backTracking(int[] solution, int queen) {
+    public boolean backTracking(int[] solution, int queen) {
+        boolean succes = false;
         if(queen < num_queens) {
             do{
                 solution[queen]++;
-                boolean valid = isValid(solution, queen);
+                boolean valid = queenIsValid(solution, queen);
+                System.out.println(Arrays.toString(solution)+" "+ (valid ? "Sol Parcial":"")+(valid && (queen == num_queens - 1) ? "SOLUTION" : ""));
                 if(valid)
-                    backTracking(solution, queen++);
-            }while(solution[queen] < num_queens-1);
+                    succes = backTracking(solution, queen+1);
+            }while(solution[queen] < num_queens-1 && !succes);
         }
+        solution[queen]=-1;
+        return succes;
     }
 
-    public boolean isValid(int[] solution, int queen) {
-        return true;
+    public boolean queenIsValid(int[] solution, int queen) {
+        boolean ok = true;
+        for (int i = 0; i < queen; i++) {
+            if ((solution[i] == solution[queen]) || (Math.abs(solution[i] - solution[queen]) == Math.abs(i - queen))) {
+                ok = false;
+                break;
+            }
+        }
+        return ok;
     }
 
     public static void main(String[] args) {
